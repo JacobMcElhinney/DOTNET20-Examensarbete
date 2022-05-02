7a72397dd5977fadc8321d74c7a223210a37564f
JacobMcElhinney <jacob.mcelhinney@gmail.com>
2022-05-02 13:17:39 +0200

    log message test added.

--- worker.powershell/Program.cs
+++ worker.powershell/Program.cs
@@ -1,10 +1,13 @@
 using worker.powershell;
 
+//Dependency injection container
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        //Add worker service to the container. 
         services.AddHostedService<Worker>();
     })
     .Build();
 
+//.NET will start the worker service.
 await host.RunAsync();
--- worker.powershell/Worker.cs
+++ worker.powershell/Worker.cs
@@ -3,7 +3,10 @@ namespace worker.powershell;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private int logEntryCount; 
 
+
+    //Called once when resolved from Dependency Injection container in Program.cs
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
@@ -13,8 +16,23 @@ public class Worker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            _logger.LogInformation($"Worker has run {logEntryCount} times", DateTimeOffset.Now);
+            logEntryCount++;
             await Task.Delay(1000, stoppingToken);
         }
+    }
+
+    //Called when the worker is started. Allows for asynchronous initialization calls.
+    public override Task StartAsync(CancellationToken cancellationToken) 
+    {
+        _logger.LogInformation("Worker starting...");
+        return base.StartAsync(cancellationToken);
+    }
+
+    //Called when the worker is stopped. Allows for cleanup calls.
+    public override Task StopAsync(CancellationToken cancellationToken) 
+    {
+        _logger.LogInformation("Worker stopping...");
+        return base.StopAsync(cancellationToken);
     }
 }
