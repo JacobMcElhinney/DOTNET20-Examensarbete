c83dc49edc44ae97e1dad140e4e2d51e24874d1e
JacobMcElhinney <jacob.mcelhinney@gmail.com>
2022-05-05 14:20:02 +0200

    before relying on dependency injection service

---
+++ worker.powershell/WorkerOptions.cs
@@ -0,0 +1,17 @@
+namespace worker.powershell 
+{
+    /// <summary>
+    /// Option class (implementation of Options pattern in .NET) used to read related configuration values in appsettings.Environment.json 
+    /// </summary>
+    public class WorkerOptions
+    {
+        //Representing a configuration value in e.g appsettings.Development.json or appsettings.json
+        public string Name { get; set; }
+    }
+
+    /* DEVELOPER NOTES:
+        Please refer to the documentation
+        https://docs.microsoft.com/en-us/dotnet/core/extensions/options
+        
+    */
+}
