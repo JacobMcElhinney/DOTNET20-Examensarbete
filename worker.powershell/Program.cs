2bebe4c0689b249593a87158a0fe72f62db9e6df
JacobMcElhinney <jacob.mcelhinney@gmail.com>
2022-05-18 09:00:06 +0200

    refactor program.cs

--- worker.powershell/Program.cs
+++ worker.powershell/Program.cs
@@ -14,25 +14,34 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddHostedService<Worker>(); //Add worker service to the container.
 
         //Register http client services 
-        services.AddHttpClient<IProcessService<ProcessStep>, ProcessService>(client =>
+        services.AddHttpClient<IProcessService<ProcessStep>, ProcessService>(name: "FlowApiClient", client =>
         {
             //Depending on the services I register I will set the base address to match the corresponding external service/API
-            client.BaseAddress = new Uri(configurationRoot["WorkerOptions:FlowAPIUrl"]); //Since Process Step relies on Flow api.
+            client.BaseAddress = new Uri(configurationRoot["WorkerOptions:FlowApiUrl"]); //Since Process Step relies on Flow api.
+            client.DefaultRequestHeaders.Add(
+                name: "User-Agent",
+                value: (configurationRoot["WorkerOptions:Name"] + "/" + configurationRoot["WorkerOptions"] + "/FlowApiClient"));
 
         }).SetHandlerLifetime(TimeSpan.FromMinutes(2));// create a standard HttpClient for each service and register services as transient so they can be injected and consumed directly without any need for additional registrations.
 
-          services.AddHttpClient<IProcessService<ProcessStep>, ProcessService>(client => //Update to new classes...
+        services.AddHttpClient<IProcessService<ProcessStep>, ProcessService>(name: "GitHubClient", configureClient: client => //Update to new classes...
         {
+
             //Depending on the services I register I will set the base address to match the corresponding external service/API
             client.BaseAddress = new Uri(configurationRoot["WorkerOptions:GitUrl"]); //Since Process Step relies on Flow api.
+            client.DefaultRequestHeaders.Add(name: "Accept", value: "");
 
+            //Use HTTP Standard header: Example: User-Agent: CERN-LineMode/2.15 libwww/2.17b3
+            client.DefaultRequestHeaders.Add(
+                name: "User-Agent",
+                value: (configurationRoot["WorkerOptions:Name"] + "/" + configurationRoot["WorkerOptions"] + "/GitHubClient"));
         }).SetHandlerLifetime(TimeSpan.FromMinutes(2));// create a standard HttpClient for each service and register services as transient so they can be injected and consumed directly without any need for additional registrations.
 
         services.AddTransient<IPowerShellService, PowerShellService>();
 
     }).Build();
 
-    
+
 
 //.NET will start the worker service.
 await host.RunAsync();
